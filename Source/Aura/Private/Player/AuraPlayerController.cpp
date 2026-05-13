14490cada7be4eba95089da98ec336907df45ae3
Liu Junyan <1768441679@qq.com>
2026-05-13 23:54:21 +0800

    修改小bug

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -17,6 +17,7 @@ void AAuraPlayerController::BeginPlay()
 	
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	check(Subsystem);
+	Subsystem->AddMappingContext( AuraContext , 0 );
 	
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Default;
