8298e80ed829f10c0d3702c5de43dfa2f88671f5
Liu Junyan <1768441679@qq.com>
2026-05-16 00:10:36 +0800

    添加简单UI，Widget组件和WidgetController，添加AuraEffectActor实现简单的拾取道具恢复血量的效果

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+﻿// See you in the battle
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,4 @@
+﻿// See you in the battle
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
