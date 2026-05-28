cd0d9cad3e20bee0747bac981b618eebb1b94c09
Liu Junyan <1768441679@qq.com>
2026-05-28 22:18:55 +0800

    添加次级属性

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -39,6 +39,11 @@ protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere , Category = "Attributes")
 	TSubclassOf<UGameplayEffect> DefaultPrimaryAttributes;
 	
-	void InitializePrimaryAttributes() const;
-
+	UPROPERTY(BlueprintReadOnly, EditAnywhere , Category = "Attributes")
+	TSubclassOf<UGameplayEffect> DefaultSecondaryAttributes;
+	
+	void ApplyEffectToSelf(const TSubclassOf<UGameplayEffect> GameplayEffectClass , const float Level) const;
+	
+	void InitializeDefaultAttributes()const ;
+	
 };
